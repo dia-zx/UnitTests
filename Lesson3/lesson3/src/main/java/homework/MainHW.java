c286bf4d1b10c6827d5bd90ce0b0f46b9ca85998
Ivan <dia_zx@mail.ru>
2023-11-25 19:53:54 +0300

    mainhw is done

--- Lesson3/lesson3/src/main/java/homework/MainHW.java
+++ Lesson3/lesson3/src/main/java/homework/MainHW.java
@@ -5,18 +5,20 @@ public class MainHW {
     // Метод проверяет, является ли целое число записанное в переменную n, чётным
     // (true) либо нечётным (false).
     public boolean evenOddNumber(int n) {
-        if (n % 2 == 0) {
+        return n % 2 == 0; // упростим запись функции
+/*        if (n % 2 == 0) {
             return true;
         } else {
             return false;
         }
+*/        
     }
 
     // HW 3.2. Нужно написать метод который проверяет, попадает ли переданное число
     // в интервал (25;100) и возвращает true, если попадает и false - если нет,
     // покрыть тестами метод на 100%
     public boolean numberInInterval(int n) {
-        return n >= 25 && n <= 100;
+        return n > 25 && n < 100;
     }
 
 }
