828551fa47a5879ff281482ce5363640ffe75181
Ivan <dia_zx@mail.ru>
2023-12-08 18:49:34 +0300

    all tests done

--- Lesson6/hw/src/test/java/ControllerTest.java
+++ Lesson6/hw/src/test/java/ControllerTest.java
@@ -23,4 +23,38 @@ public class ControllerTest {
                         "Средние значения равны",
                 out);
     }
+
+    @Test
+    @DisplayName("Controller.Start() контроль на \"Первый список имеет большее среднее значение\" по среднему арифметическому списков")
+    public void userInputDataList_A1LessTest() {
+        String out = TestTools.ioListen(
+                () -> Controller.Start(),
+                "10 2 3 5 6" + System.lineSeparator() + "3 1 2 5 6" + System.lineSeparator()).trim();
+
+        assertEquals(
+                        "Ввод первого списка чисел (double)"            + System.lineSeparator() +
+                        "Вводите значения чисел списка через <пробел>"  + System.lineSeparator() +
+                        "*****************************************"     + System.lineSeparator() +
+                        "Ввод второго списка чисел (double)"            + System.lineSeparator() +
+                        "Вводите значения чисел списка через <пробел>"  + System.lineSeparator() +
+                        "Первый список имеет большее среднее значение",
+                out);
+    }    
+
+    @Test
+    @DisplayName("Controller.Start() контроль на \"Второй список имеет большее среднее значение\" по среднему арифметическому списков")
+    public void userInputDataList_A1GreatTest() {
+        String out = TestTools.ioListen(
+                () -> Controller.Start(),
+                "10 2 3 5 6" + System.lineSeparator() + "3 20 2 5 6" + System.lineSeparator()).trim();
+
+        assertEquals(
+                        "Ввод первого списка чисел (double)"            + System.lineSeparator() +
+                        "Вводите значения чисел списка через <пробел>"  + System.lineSeparator() +
+                        "*****************************************"     + System.lineSeparator() +
+                        "Ввод второго списка чисел (double)"            + System.lineSeparator() +
+                        "Вводите значения чисел списка через <пробел>"  + System.lineSeparator() +
+                        "Второй список имеет большее среднее значение",
+                out);
+    }        
 }
--- Lesson6/hw/src/test/java/TestTools.java
+++ Lesson6/hw/src/test/java/TestTools.java
@@ -4,6 +4,12 @@ import java.io.InputStream;
 import java.io.PrintStream;
 
 public class TestTools {
+    /**
+     * Подменяет потоки ввода и вывода для возможности тестирования и анализа
+     * @param runnable Запускаемый метод для тестирования потоков
+     * @param ForInputStream данная строка передается в поток ввода, имитируя пользовательский ввод
+     * @return строка записанная потоком вывода (для тестирования выводимых сообщений)
+     */
     public static String ioListen(Runnable runnable, String ForInputStream) {
         InputStream in_last = System.in;
         ByteArrayInputStream io = new ByteArrayInputStream(ForInputStream.getBytes());
